2443dc7d4b43b25c09d8985d0b5e310dd2250e6a
dacaiguoguo <dacaiguoguo@163.com>
2014-01-23 15:02:13 +0800

    add +//  UIView+YGLoadViewFromXib

---
+++ UIView+YGLoadViewFromXib.h
@@ -0,0 +1,18 @@
+//
+//  UIView+YGLoadViewFromXib.h
+//  LvmmPad
+//
+//  Created by dacaiguoguo on 14-1-23.
+//  Copyright (c) 2014年 zhouyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (YGLoadViewFromXib)
+
++ (instancetype)loadViewFromXib:(NSString *)xibName viewClass:(Class)viewClass atIndex:(NSUInteger)index;
+
++ (instancetype)loadViewFromXib:(NSString *)xibName viewClass:(Class)viewClass;
+
++ (instancetype)loadViewFromXib:(NSString *)xibName;
+@end
---
+++ UIView+YGLoadViewFromXib.m
@@ -0,0 +1,41 @@
+//
+//  UIView+YGLoadViewFromXib.m
+//  LvmmPad
+//
+//  Created by dacaiguoguo on 14-1-23.
+//  Copyright (c) 2014年 zhouyi. All rights reserved.
+//
+
+#import "UIView+YGLoadViewFromXib.h"
+
+@implementation UIView (YGLoadViewFromXib)
+
++ (instancetype)loadViewFromXib:(NSString *)xibName viewClass:(Class)viewClass atIndex:(NSUInteger)index{
+    UIView *ret = nil;
+    int zeroIndex = 0;
+    NSArray *nibs = [[NSBundle mainBundle] loadNibNamed:xibName owner:nil options:nil];
+    for (int i=0; i<nibs.count; i++) {
+        UIView *test =  nibs[i];
+        if (test.class == viewClass) {
+            if (index == zeroIndex) {
+                ret = test;
+                break;
+            }
+            zeroIndex++;
+            
+        }
+    }
+    return ret;
+}
+
+
++ (instancetype)loadViewFromXib:(NSString *)xibName viewClass:(Class)viewClass{
+    
+    return [self loadViewFromXib:xibName viewClass:viewClass atIndex:0];
+}
+
++ (instancetype)loadViewFromXib:(NSString *)xibName{
+    
+    return [self loadViewFromXib:xibName viewClass:[self class] atIndex:0];
+}
+@end
